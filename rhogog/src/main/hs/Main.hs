f71c605575f0f952184b396b52c2fc4a9c287b3b
Pepijn <pepijn.kokke@gmail.com>
2013-04-22 02:46:39 +0200

    wrote rhogog parser for the new lexicon format.

---
+++ rhogog/src/main/hs/Main.hs
@@ -0,0 +1,9 @@
+import Base
+import Parsing
+import Printing
+
+import Data.String.Utils
+import Text.ParserCombinators.UU.Utils (runParser)
+
+main :: IO ()
+main = getContents >>= print . runParser "StdIn" pLx
