c626da0ff9d11c7667577ac0d295c4fcf9cb16b4
Wen Kokke <wenkokke@users.noreply.github.com>
2013-04-23 23:00:36 +0200

    changes:  - updated and corrected the rhogog parser     * most notably adding optional type annotations for abstractions;     * and corrected the type parser to match my intuition that (->) binds stronger       than regular type binding (i.e. et -> et now parses to the type (et)et).  - started implementing extended hinley-milner type inference for the lexicon.
    Signed-off-by: Pepijn <wen.kokke@gmail.com>

--- rhogog/src/main/hs/Parsing.hs
+++ rhogog/src/main/hs/Parsing.hs
@@ -8,6 +8,7 @@ import Text.ParserCombinators.UU
 import Text.ParserCombinators.UU.BasicInstances
 import Text.ParserCombinators.UU.Utils
 
+-- |Parses a lexicon in the extended syntax.
 pLx :: Parser Lx
 pLx = Lx_Lx <$ pSpaces <*> pMany pSt
 
@@ -28,35 +29,52 @@ pAn = pPubl <<|> pPriv <<|> pAuto
 
 -- |Parses types in the extended lexicon syntax.
 pTy :: Parser Ty
-pTy = pApp <|> pVar
+pTy = pAll <<|> pApp2
   where
-  pVar = lexeme $ Ty_Var <$> pLower <<|> pParens pTy
-  pApp = Ty_App <$> pVar <??> pSym <*> pTy
-  pSym = flip const <$> pSymbol "->"
+  pVar  = Ty_Var <$> pTyVar <<|> pParens pTy
+  pApp1 = foldr1 Ty_App <$> pList1Sep (pure []) pVar
+  pApp2 = foldr1 Ty_App <$> pList1Sep (pSymbol "->") pApp1
+  pAll  = uncurry (foldr Ty_All) <$ pSymbol "!" <*> pTyVars
+  
+-- |Parses a type variable in the extended lexicon syntax.
+pTyVar :: Parser TyVar
+pTyVar = lexeme $ pLower
+
+-- |Parses type variables in the extended lexicon syntax.
+pTyVars :: Parser (Ty,[TyVar])
+pTyVars = flip (,) <$> pList1Sep pSpaces pTyVar <* pDot <*> pTy
 
 -- |Parses terms in the extended lexicon syntax.
 pTm :: Parser Tm
 pTm = pLambda <<|> pIota <<|> pForall <<|> pExists <<|> pTerm
   where
-  pVar        = lexeme $ Tm_Var <$> pIdent <<|> pParens pTm
-  pApp        = foldl1 Tm_App <$> pList1Sep pSpaces pVar
-  pOp x op e  = foldl1 (Tm_App . Tm_App (Tm_Var x)) <$> pList1Sep op e
-  pConj       = pOp "AND"     (pSymbol "/\\") pApp
-  pDisj       = pOp "OR"      (pSymbol "\\/") pConj
-  pImpl       = pOp "IMPLIES" (pSymbol "->")  pDisj
-  pEquiv      = pOp "EQ"      (pSymbol "==")  pImpl
-  pTerm       = pEquiv
   
-  pIdents     :: Parser (Tm,[Var])
-  pIdents     = flip (,) <$> pList1Sep pSpaces pIdent <* pDot <*> pTm
-  absapp      :: Var -> (Tm,[Var]) -> Tm
-  absapp f    = uncurry (foldr (\x -> Tm_App (Tm_Var f) . Tm_Lam x))
-  pForall     :: Parser Tm
-  pForall     = absapp "FORALL" <$ pSymbol "!"  <*> pIdents
-  pExists     = absapp "EXISTS" <$ pSymbol "?"  <*> pIdents
-  pIota       = absapp "IOTA"   <$ pSymbol "i"  <*> pIdents
-  pLambda     = uncurry (foldr Tm_Lam) <$ pSymbol "\\" <*> pIdents
+  -- |Parses simple terms.
+  pTerm      = pEquiv
+  pImpl      = pOp "IMPLIES" (pSymbol "->")  pDisj
+  pEquiv     = pOp "EQ"      (pSymbol "==")  pImpl
+  pDisj      = pOp "OR"      (pSymbol "\\/") pConj
+  pConj      = pOp "AND"     (pSymbol "/\\") pApp
+  pOp x op e = foldl1 (Tm_App . Tm_App (Tm_Var x)) <$> pList1Sep op e
+  pApp       = foldl1 Tm_App <$> pList1Sep pSpaces pVar
+  pVar       = lexeme $ Tm_Var <$> pIdent <<|> pParens pTm
+  
+  -- |Parses quantifying terms.
+  pForall    = quantify "FORALL" <$ pSymbol "!"  <*> pTyIdents
+  pExists    = quantify "EXISTS" <$ pSymbol "?"  <*> pTyIdents
+  pIota      = quantify "IOTA"   <$ pSymbol "i"  <*> pTyIdents
+  pLambda    = uncurry (foldr $ uncurry Tm_Lam) <$ pSymbol "\\" <*> pTyIdents
+  quantify f = uncurry (foldr $ (Tm_App (Tm_Var f).) . uncurry Tm_Lam)
   
 -- |Parses identifiers in the extended lexicon syntax.
 pIdent :: Parser Var
 pIdent = (:) <$> pLetter <*> pMany (pLetter <|> pDigit <|> pSym '_')
+
+-- |Parses an explicitly typed identifier in the extended lexicon syntax.
+pTyIdent :: Parser (Var,MbTy)
+pTyIdent = (,) <$> pIdent <*> (pMaybe $ pSymbol ":" *> pTy)
+
+-- |Parses a sequence of typed identifiers.
+pTyIdents :: Parser (Tm,[(Var,MbTy)])
+pTyIdents = flip (,) <$> pList1Sep s pTyIdent <* pDot <*> pTm
+  where s = pSym ' '
