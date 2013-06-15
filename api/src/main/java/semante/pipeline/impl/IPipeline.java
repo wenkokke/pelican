d412df3cd4174d13795fdecad5b5db059471d441
Pepijn Kokke <pepijn.kokke@gmail.com>
2013-06-15 22:52:37 +0200

    beta reduction and annotation tree flattening works reasonably well now. there are two simple bugs: - beta reduction doesn't recurse properly and therefore sometimes needs to be run multiple times; - error messages are a bit unclear, as there are usually many possibilities;
    Signed-off-by: Pepijn Kokke <pepijn.kokke@gmail.com>

--- api/src/main/java/semante/pipeline/impl/IPipeline.java
+++ api/src/main/java/semante/pipeline/impl/IPipeline.java
@@ -1,22 +1,17 @@
 package semante.pipeline.impl;
 
-import static com.google.common.collect.Lists.transform;
 import static lombok.AccessLevel.PRIVATE;
 
 import java.io.FileNotFoundException;
 import java.util.List;
 
 import lambdacalc.DeBruijn;
-import lambdacalc.Expr;
 import lambdacalc.STL;
 import lombok.Delegate;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import lombok.experimental.FieldDefaults;
-import predcalc.ExprForm;
-import predcalc.util.ILowerLambda;
 import semante.lexicon.RichLexicon;
-import semante.lexicon.Word;
 import semante.pipeline.Annotation;
 import semante.pipeline.BinaryTree;
 import semante.pipeline.Category;
@@ -26,9 +21,6 @@ import semante.pipeline.TestCaseCreator;
 import semante.settings.Settings;
 
 import com.google.common.base.Function;
-import com.google.common.base.Functions;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
 
 @RequiredArgsConstructor
@@ -43,57 +35,34 @@ public final class IPipeline implements Pipeline {
 	
 	@Override
 	public final <ID> Result<ID> prove(
-		final BinaryTree<ID, Annotation> txt,
-		final BinaryTree<ID, Annotation> hyp,
+		final BinaryTree<ID, Annotation> text,
+		final BinaryTree<ID, Annotation> hypo,
 		final String subsumptions) throws FileNotFoundException {
 		
-		// lookup annotations in the lexicon.
-		val lookup = IBinaryTree.functor(
-			Functions.<ID> identity(), 
-			new Function<Annotation,Word>() {
-				
-				@Override
-				public final Word apply(final Annotation a) {
-					return a.accept(new Annotation.Visitor<Word>() {
-
-						@Override
-						public final Word annotation(
-								final String text,
-								final String annotation) {
-							return lexicon.getEntry(text, annotation);
-						}
-					});
-				}
-			});
-		val anntxt = txt.accept(lookup);
-		val annhyp = hyp.accept(lookup);
+		val flattener = new IFlattenTree<ID>(stl,lexicon);
+		val flatTextM = flattener.flatten(text);
+		if (flatTextM.isLeft()) return flatTextM.getLeft();
+		val flatHypoM = flattener.flatten(hypo);
+		if (flatHypoM.isLeft()) return flatHypoM.getLeft();
+		val flatTexts = flatTextM.getRight();
+		val flatHypos = flatTextM.getRight();
 		
-		// flatten trees into lambda expressions.
-		val flatten = new IFlattenTree<ID>(stl);
-		val exprTxt = flatten.flatten(anntxt);
-		if (exprTxt.isLeft()) { return exprTxt.getLeft(); }
-		val exprHyp = flatten.flatten(annhyp);
-		if (exprHyp.isLeft()) { return exprHyp.getLeft(); }
+		val reducer = new Function<DeBruijn,DeBruijn>() {
+			@Override
+			public final DeBruijn apply(final DeBruijn expr) {
+				// TODO apparently there is a small bug in beta reduction
+				// 		as we need two reductions in the case of jan01;
+				return stl.etaReduce(stl.betaReduce(stl.betaReduce(expr)));
+			}
+		};
+		val redTexts = Lists.transform(flatTexts, reducer);
+		val redHypos = Lists.transform(flatHypos, reducer);
 		
-		// beta-reduce the expressions.
-		val betaReduce =
-			new Function<DeBruijn, DeBruijn>() {
-				@Override
-				public final DeBruijn apply(DeBruijn input) {
-					return stl.betaReduce(input);
-				}
-			};
-		val reducedTxt = transform(exprTxt.getRight(),betaReduce);
-		val reducedHyp = transform(exprHyp.getRight(),betaReduce);
-		
-		val uniqueTxt = ImmutableList.copyOf(ImmutableSet.copyOf(reducedTxt));
-		val uniqueHyp = ImmutableList.copyOf(ImmutableSet.copyOf(reducedHyp));
-		
-		for (val nub: uniqueTxt) {
-			System.err.println("txt:"+stl.format(stl.betaReduce(stl.fromDeBruijn(nub))));
+		for (val redText: redTexts) {
+			System.out.println(stl.format(stl.fromDeBruijn(redText)));
 		}
-		for (val nub: uniqueHyp) {
-			System.err.println("hyp:"+stl.format(stl.betaReduce(stl.fromDeBruijn(nub))));
+		for (val redHypo: redHypos) {
+			System.out.println(stl.format(stl.fromDeBruijn(redHypo)));
 		}
 		
 		// TODO implement smasher and prover9 parts of pipeline
--- lambdacalc/src/main/java/lambdacalc/impl/IIndex.java
+++ lambdacalc/src/main/java/lambdacalc/impl/IIndex.java
@@ -1,11 +1,20 @@
 package lambdacalc.impl;
 
+import static lombok.AccessLevel.PRIVATE;
 import lambdacalc.Index;
 import lambdacalc.Type;
-import lombok.experimental.Value;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 
-@Value
+@Getter
+@RequiredArgsConstructor
+@EqualsAndHashCode(callSuper=false)
+@FieldDefaults(makeFinal=true,level=PRIVATE)
 public final class IIndex implements Index {
+	
 	Integer	index;
 	Type	type;
+	
 }
