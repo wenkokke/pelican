5bcbe4de24203cce124d1204c4e10f1cc30ceb33
Benno Kruit <bennokr@gmail.com>
2013-06-29 16:32:23 +0200

    Fixed smasher

--- predcalc/src/main/java/predcalc/util/ILowerLambda.java
+++ predcalc/src/main/java/predcalc/util/ILowerLambda.java
@@ -27,15 +27,27 @@ public class ILowerLambda implements LowerLambda {
 			val fun2 = fun.accept(rewriter);
 			val arg2 = arg.accept(rewriter);
 			val app = lcalc.getExprBuilder().application(fun2, arg2);
-			// If there's a (et)et constant applied to an et constant, we replace it
-			if (lcalc.typeOf(fun2).equals(Types.ET_ET) && lcalc.typeOf(arg2).equals(Types.ET)) {
-				if (fun2.accept(isConstant) && arg2.accept(isConstant)) {
-					return lcalc.getExprBuilder().variable(buildSymbol("p"+app.hashCode(), Types.ET));
+			
+			val ee = lcalc.getTypeBuilder().function(Types.E, Types.E); // TODO BUGFIX
+			val e_et_et = lcalc.getTypeBuilder().function(Types.E, Types.ET_ET);
+			
+			if (lcalc.typeOf(fun2).equals(Types.ET_ET) && (lcalc.typeOf(arg2).equals(Types.ET) || lcalc.typeOf(arg2).equals(ee))) {
+				// If there's a (et)et constant applied to an et constant, we replace it by a fresh et constant
+				if (fun2.accept(isConstant) /*&& arg2.accept(isConstant)*/) {
+					return lcalc.getExprBuilder().variable(buildSymbol(app.accept(namer), Types.ET));
 				} else {
 					throw new HigherOrderError("Non-constants of type (et)et and (et): "+fun2+" "+arg2);
 				}
+			} else if (lcalc.typeOf(fun2).equals(e_et_et) && lcalc.typeOf(arg2).equals(Types.E)) {
+				// If there's a e(et)et constant applied to an e constant, we replace it by a fresh (et)et constant
+				if (fun2.accept(isConstant) && arg2.accept(isConstant)) {
+					return lcalc.getExprBuilder().variable(buildSymbol(app.accept(namer), Types.ET_ET));
+				} else {
+					throw new HigherOrderError("Non-constants of type (et)et and (et): "+fun2+" "+arg2);
+				}
+			} else {
+				return app;
 			}
-			return app;
 		}
 		
 		@Override
@@ -74,8 +86,35 @@ public class ILowerLambda implements LowerLambda {
 		};
 	}
 	
+	private Visitor<String> namer = new Visitor<String>() {
+		@Override public String abstraction(Symbol s, Expr body) { return body.accept(namer); }
+		@Override public String application(Expr fun, Expr arg) {
+			if (arg.accept(new Visitor<Boolean>() {
+				@Override public Boolean abstraction(Symbol s, Expr body) { 
+					return body.accept(new Visitor<Boolean>() {
+						@Override public Boolean abstraction(Symbol s, Expr body) { return false; }
+						@Override public Boolean application(Expr fun, Expr arg) { return false; }
+						@Override public Boolean variable(Symbol s) { return s.getType().equals(Types.T); }
+						});
+				}
+				@Override public Boolean application(Expr fun, Expr arg) { return false; }
+				@Override public Boolean variable(Symbol s) { return false; }
+				})) {
+				return fun.accept(namer);
+			}
+			return fun.accept(namer)+((arg.accept(isConstant) && lcalc.typeOf(arg).equals(Types.E))? "" : "_"+arg.accept(namer));
+		}
+		@Override public String variable(Symbol s) { return s.getName(); }
+	};
+	
+	String name(final Expr exp) {
+		return "p"+exp.hashCode();
+	}
+	
 	public Expr rewrite(Expr a) {
-		return a.accept(rewriter);
+		Expr o = a.accept(rewriter);
+//		System.err.println("rewrote " + lcalc.format(a) + " to " + lcalc.format(o));
+		return o;
 	}
 
 }
