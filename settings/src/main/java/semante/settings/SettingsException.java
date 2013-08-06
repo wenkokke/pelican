7f73fdcf190b588a7486ef1e7c0200a0a3a85bd4
Assaf Toledo <assaf_t@yahoo.com>
2013-08-06 16:55:40 +0200

    Suppress serialization warning

--- settings/src/main/java/semante/settings/SettingsException.java
+++ settings/src/main/java/semante/settings/SettingsException.java
@@ -2,6 +2,7 @@ package semante.settings;
 
 import java.util.Arrays;
 
+@SuppressWarnings("serial")
 public final class SettingsException extends RuntimeException {
 
 	public SettingsException(String setting) {
