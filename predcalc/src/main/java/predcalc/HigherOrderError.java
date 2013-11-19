44fc178ad33c4b5f5f6e652b169532914cb7eb64
Assaf Toledo <assaf_t@yahoo.com>
2013-11-19 05:50:11 +0100

    Avoid warning on serialization

--- predcalc/src/main/java/predcalc/HigherOrderError.java
+++ predcalc/src/main/java/predcalc/HigherOrderError.java
@@ -1,5 +1,6 @@
 package predcalc;
 
+@SuppressWarnings("serial")
 public final class HigherOrderError extends IllegalArgumentException {
 	public HigherOrderError(final String msg, final Object... args) {
 		super(String.format(msg,args));
